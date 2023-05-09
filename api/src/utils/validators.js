bc0e707861321ff12485b1dfa01d33ea538ee1c5
Jere <jere_newells@hotmail.com>
2023-05-09 08:41:09 -0300

    validator

--- api/src/utils/validators.js
+++ api/src/utils/validators.js
@@ -11,7 +11,7 @@ const getIdValidator = (req, res, next) => {
   const { idVideogame } = req.params;
   const uuidRegex =
     /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89aAbB][0-9a-f]{3}-[0-9a-f]{12}$/;
-  if (isNaN(idVideogame) || !uuidRegex.test(idVideogame))
+  if (isNaN(idVideogame) && !uuidRegex.test(idVideogame))
     return res.status(400).json(ERROR.GET_ID);
   next();
 };
