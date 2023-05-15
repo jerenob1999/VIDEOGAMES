ea96c3ed1ba404f610aa02faaa764a7222c0fa4e
Jere <jere_newells@hotmail.com>
2023-05-15 18:40:25 -0300

    working on genres filter

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -1,60 +1,118 @@
-import { filterVideogames, orderVideogames, filterBySource, setSource } from "../../redux/actions";
+import { setSource } from "../../redux/actions";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import { getGenres } from "../../redux/actions";
 
 const Filter = () => {
   const dispatch = useDispatch();
-  const {genres , videogames, source} = useSelector(state => state);
-  const [displayDropdown,setDisplayDrowdown] = useState(false)
-  const [origin , setOrigin] = useState("")
-  
+  const [displayDropdown, setDisplayDropdown] = useState({
+    genre:false,
+    source:false
+  });
 
-  const handleDropdown = () => {
-    setDisplayDrowdown(!displayDropdown)
-  }
+
+
+
+  const [origin, setOrigin] = useState("");
+  const {genres} = useSelector(state => state)
+  const [genre, setGenre] = useState("");
+
+  const handleDropdown = (event) => {
+    const property = event.target.name
+    setDisplayDropdown({...displayDropdown,[property]:!displayDropdown[property]});
+  };
 
   useEffect(() => {
-    if(origin !== "") {
-      dispatch(setSource(origin))
+    if (!genres.length) {
+        dispatch(getGenres())
     }
-  },[dispatch,origin])
+}, [])
+
+  useEffect(() => {
+    if (origin !== "") {
+      dispatch(setSource(origin));
+    }
+  }, [dispatch, origin]);
 
   return (
+    <div>
+
+      <section>
+      <div onClick={handleDropdown} >
+        <input
+          type="text"
+          readOnly
+          placeholder="Filter by genre"
+          value={genre}
+          name="genre" 
+        />
+      </div>
+      {displayDropdown.genre ? (
+        <div>
+          {genres.map(genre => {
+            return (
+              <div key={genre.id} onClick={() => {
+                setGenre(genre.name)
+                handleDropdown({ target: { name: "genre" } }) }}>{genre.name}</div>
+            )
+          })}
+        </div>
+      ): null}
+      </section>
+
+
 
     <section>
       <div onClick={handleDropdown}>
         <input
-        type="text"
-        readOnly
-        placeholder="Filter by source"
-        value={origin}        
+          type="text"
+          readOnly
+          placeholder="Filter by source"
+          value={origin}
+          name="source"
         />
       </div>
 
-      {displayDropdown ? (
-        <div>
-          <div onClick={() => {
-         setOrigin("API")
-         handleDropdown() }} >API</div>
+      {displayDropdown.source ? (
+        <div name="source">
+          <div
+             
+             onClick={() => {
+              setOrigin("API");
+              handleDropdown({ target: { name: "source" } });
+            }}
+          >
+            API
+          </div>
 
+          <div
+            onClick={() => {
+              setOrigin("CREATED");
+              handleDropdown({ target: { name: "source" } });
+            }}
+          >
+            CREATED
+          </div>
 
-          <div onClick={() => {
-            setOrigin("CREATED")
-            handleDropdown()
-          }} >CREATED</div>
+          <div
+            onClick={() => {
+              setOrigin("ALL");
+              handleDropdown({ target: { name: "source" } });
+            }}
+          >
+            ALL
+          </div>
         </div>
       ) : null}
-
     </section>
-//  
+    </div>
   );
 };
 
 export default Filter;
 
-
-
-{/* <div>
+{
+  /* <div>
 //    <label htmlFor="source-select">Selecciona el origen:</label>
 //    <select id="source-select" value={source} onChange={handleSource}>
 //    <option value="TODOS">Todos</option>
@@ -73,11 +131,10 @@ export default Filter;
 
 
       
-//     </div> */}
-
+//     </div> */
+}
 
 // const [orderBy, setOrderBy] = useState("")
-
 
 // const handleOrder = (event) => {
 //   const {name, value} = event.target;
